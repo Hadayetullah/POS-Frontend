abbe477a43ef8652fcc013e942d6415db4ec33dc
Hadayetullah <hadayetullah30@gmail.com>
2025-04-27 12:46:52 +0600

    Update

--- pos-frontend/lib/rootReducer.js
+++
@@ -1,9 +0,0 @@
-// src/app/rootReducer.js
-import { combineReducers } from "@reduxjs/toolkit";
-import counterSlice from "./features/counterSlice";
-
-const rootReducer = combineReducers({
-  counter: counterSlice,
-});
-
-export default rootReducer;
